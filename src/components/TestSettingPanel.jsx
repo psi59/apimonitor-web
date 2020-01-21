cca6de751fe2491fd2fa9c1a463eda15333af1e0
realsangil <tkddlf59@gmail.com>
2020-01-21 20:31:39 +0900

    Remove tab in test setting panel

--- src/components/TestSettingPanel.jsx
+++ src/components/TestSettingPanel.jsx
@@ -20,25 +20,10 @@ const tabs = [
 class TestSettingPanel extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedTab: parameterTab,
-        }
     }
-
-    selectTab = tab => e => {
-        this.setState({
-            selectedTab: tab
-        })
-    };
-
-    getTabClass = tab => {
-        const { selectedTab } = this.state;
-        return (selectedTab === tab) ? "is-active" : "";
-    };
 
     render() {
         const { testStore } = this.props;
-        const { selectedTab } = this.state;
         const { test } = testStore;
         const panelStyle = {
           border: "1px solid #e5e5e5"
@@ -48,59 +33,33 @@ class TestSettingPanel extends React.Component {
         };
         return (
             test && <div className="panel is-shadowless" style={panelStyle}>
-                <div className="panel-block">
-                    <div className="tabs is-boxed">
-                        <ul>
-                            <li
-                                className={this.getTabClass(parameterTab)}
-                                onClick={this.selectTab(parameterTab)}
-                            >
-                                <a className="has-text-weight-bold has-text-link">Parameters</a>
-                            </li>
-                            <li
-                                className={this.getTabClass(assertionTab)}
-                                onClick={this.selectTab(assertionTab)}
-                            >
-                                <a className="has-text-weight-bold has-text-link">Assertions</a>
-                            </li>
-                            <li
-                                className={this.getTabClass(settingTab)}
-                                onClick={this.selectTab(settingTab)}
-                            >
-                                <a className="has-text-weight-bold has-text-link">Settings</a>
-                            </li>
-                        </ul>
-                    </div>
+                <div
+                    className="panel-block"
+                    style={panelBlockStyle}
+                >
+                    <ParameterPanel
+                        header="Header parameters"
+                        parameterName={"header"}
+                    />
                 </div>
-                {(selectedTab === parameterTab) && <div>
-                    <div
-                        className="panel-block"
-                        style={panelBlockStyle}
-                    >
-                        <ParameterPanel
-                            header="Header parameters"
-                            parameterName={"header"}
-                        />
-                    </div>
-                    <div
-                        className="panel-block"
-                        style={panelBlockStyle}
-                    >
-                        <ParameterPanel
-                            header="Query parameters"
-                            parameterName={"query"}
-                        />
-                    </div>
-                    <div
-                        className="panel-block"
-                        style={panelBlockStyle}
-                    >
-                        <BodyParameterPanel
-                            header="Request body"
-                            parameterName={"body"}
-                        />
-                    </div>
-                </div>}
+                <div
+                    className="panel-block"
+                    style={panelBlockStyle}
+                >
+                    <ParameterPanel
+                        header="Query parameters"
+                        parameterName={"query"}
+                    />
+                </div>
+                <div
+                    className="panel-block"
+                    style={panelBlockStyle}
+                >
+                    <BodyParameterPanel
+                        header="Request body"
+                        parameterName={"body"}
+                    />
+                </div>
             </div>
         );
     }
